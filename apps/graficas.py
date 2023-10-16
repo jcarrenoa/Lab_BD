315ce971fc6b670852080d26073ff5b594b61a84
Aaron Rodriguez <98917564+jcarrenoa@users.noreply.github.com>
2023-10-16 14:32:16 -0500

    Update graficas.py

--- apps/graficas.py
+++ apps/graficas.py
@@ -10,7 +10,7 @@ def app():
 
         def __init__(self):
             self.apps = []
-            self.connection = pyodbc.connect('Driver={ODBC Driver 18 for SQL Server};Server=tcp:aaron02.database.windows.net,1433;Database=US_Accidents;Uid=administrador;Pwd=a123456@;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;')
+            self.connection = pyodbc.connect('DRIVER={ODBC Driver 18 for SQL Server};SERVER=tcp:aaron02.database.windows.net,1433;DATABASE=US_Accidents;UID=administrador;PWD=a123456@')
             query_1 = '''with paises(id_pais) as 
             (
                 select distinct a.State
